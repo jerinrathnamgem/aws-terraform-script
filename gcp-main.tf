018c7709b85eb435a1b7d2c750fdcf1b2dbcc495
JerinRathnam <jerinrathnam.gem@gmail.com>
2024-03-04 11:44:04 +0530

    change gcp project service

--- gcp-main.tf
+++ gcp-main.tf
@@ -2,8 +2,9 @@
 resource "google_project_service" "this" {
   for_each = var.gcp_deployment ? toset(var.gcp_service_list) : []
 
-  project = var.project_id
-  service = each.key
+  project                    = var.project_id
+  service                    = each.key
+  disable_dependent_services = true
 
   disable_on_destroy = true
 }
